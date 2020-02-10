f0e76843bc65f0802fd7c04476ac0c55405310df
pranav <59198870+Pranav2008-droid@users.noreply.github.com>
2020-02-10 06:44:51 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,84 +1,183 @@
-var strokeWeightInput, submit;
-var strokeColorInput;
-
-var strokeValue;
-
-var bginput;
-var bgcolor = 255;
-
-var clear;
-
-var canvas1;
-var canvas2;
-
-strokeWeightValue = 0;
-
-function setup(){
-    strokeWeightInput = createInput();
-    strokeWeightInput.position(150, 20);
-
-    strokeColorInput = createInput();
-    strokeColorInput.position(150, 80);
-
-    bginput = createInput();
-    bginput.position(150, 140);
-
-    submit = createButton('submit');
-    submit.position(windowWidth / 20,windowHeight/ 4);
-    submit.mousePressed(Submit)
-
-    clear = createButton('clear');
-    clear.position(windowWidth / 8, windowHeight/ 4);
-    clear.mousePressed(Clear);
-    // createCanvas(windowWidth, windowHeight);
-
-    canvas1 = createCanvas(windowWidth, windowHeight)
-    background(52, 255);
-    canvas2 = createCanvas(windowWidth, windowHeight)
+// Daniel Shiffman
+// http://codingtra.in
+// http://patreon.com/codingtrain
+// Code for: https://youtu.be/RUSvMxxm_Jo
 
 
-    canvas2.strokeWeight(2);
-
-    canvas2.stroke(255)
-    text("stroke thickness: ", 10, 20, 150, 150)
-    text("stroke color(red, green , blue, yellow etc):" , 10, 60, 150, 150)
-    text("background color(red, green , blue, yellow etc):" , 10, 120, 150, 150)
+var bgcolor = "white";
+var strokeinp = "black";
+var strokeWeightinp = 5;
+var database;
 
 
+var drawing = [];
+var currentPath = [];
+var isDrawing = false;
 
+askPrompt();
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+
+  canvas.mousePressed(startPath);
+  canvas.parent('canvascontainer');
+  canvas.mouseReleased(endPath);
+
+  var saveButton = select('#saveButton');
+  saveButton.mousePressed(saveDrawing);
+
+  var changeSettings = createButton('change Settings');
+  changeSettings.mousePressed(askPrompt);
+
+  var clearButton = select('#clearButton');
+  clearButton.mousePressed(clearDrawing);
+
+
+  database = firebase.database();
+
+  var params = getURLParams();
+  console.log(params);
+  if (params.id) {
+    console.log(params.id);
+    showDrawing(params.id);
+  }
+
+  var ref = database.ref('drawings');
+  ref.on('value', gotData, errData);
 }
 
-function draw(){
-
-    strokeWeight(strokeWeightValue);
-    // stroke(strokeValue)
-    canvas2.text("stroke thickness: ", 10, 20, 150, 150)
-    canvas2.text("stroke color(red, green , blue, yellow etc):" , 10, 60, 150, 150)
-    canvas2.text("background color(red, green , blue, yellow etc):" , 10, 120, 150, 150)
-
+function startPath() {
+  isDrawing = true;
+  currentPath = [];
+  drawing.push(currentPath);
 }
 
-function mouseDragged(){
-    canvas1.stroke(strokeValue);
-    canvas1.line(mouseX, mouseY, pmouseX, pmouseY);
-}
-
-function Submit(){
-
-    canvas2.text("stroke color(red, green , blue, yellow etc)", 50, 60, 150, 170)
-
-    canvas2.text("stroke thickness", 50, 20, 150, 150)
-
-    let weightvalue = strokeWeightInput.value();
-    strokeWeightValue = weightvalue;
-    let colorvalue = strokeColorInput.value();
-    strokeValue = colorvalue
-    bgcolor = bginput.value();
-    canvas1.background(bgcolor);
+function endPath() {
+  isDrawing = false;
 }
 
 
 
-function Clear(){
-    background(bgcolor);
+//FunctionDraw
+//FunctionDraw
+//FunctionDraw
+//FunctionDraw
+//FunctionDraw
+//FunctionDraw
+//FunctionDraw
+//FunctionDraw
+//FunctionDraw
+//FunctionDraw
+//FunctionDraw
+//FunctionDraw
+//FunctionDraw
+//FunctionDraw
+//FunctionDraw
+function draw() {
+  background(bgcolor);
+
+  if (isDrawing) {
+    var point = {
+      x: mouseX,
+      y: mouseY
+    };
+    currentPath.push(point);
+  }
+
+  stroke(strokeinp);
+  strokeWeight(strokeWeightinp);
+  noFill();
+  for (var i = 0; i < drawing.length; i++) {
+    var path = drawing[i];
+    beginShape();
+    for (var j = 0; j < path.length; j++) {
+      vertex(path[j].x, path[j].y);
+    }
+    endShape();
+  }
+}
+//FunctionDraw
+//FunctionDraw
+//FunctionDraw
+//FunctionDraw
+//FunctionDraw
+//FunctionDraw
+//FunctionDraw
+//FunctionDraw
+//FunctionDraw
+//FunctionDraw
+//FunctionDraw
+//FunctionDraw
+//FunctionDraw
+//FunctionDraw
+//FunctionDraw
+
+function saveDrawing() {
+  var ref = database.ref('drawings');
+  var data = {
+    name: 'Pranav',
+    drawing: drawing
+  };
+  var result = ref.push(data, dataSent);
+  console.log(result.key);
+
+  function dataSent(err, status) {
+    console.log(status);
+  }
+}
+
+function gotData(data) {
+  // clear the listing
+  var elts = selectAll('.listing');
+  for (var i = 0; i < elts.length; i++) {
+    elts[i].remove();
+  }
+
+  var drawings = data.val();
+  var keys = Object.keys(drawings);
+  for (var i = 0; i < keys.length; i++) {
+    var key = keys[i];
+    //console.log(key);
+    var li = createElement('li', '');
+    li.class('listing');
+    var ahref = createA('#', key);
+    ahref.mousePressed(showDrawing);
+    ahref.parent(li);
+
+    var perma = createA('?id=' + key, 'permalink');
+    perma.parent(li);
+    perma.style('padding', '4px');
+
+    li.parent('drawinglist');
+  }
+}
+
+function errData(err) {
+  console.log(err);
+}
+
+function showDrawing(key) {
+  //console.log(arguments);
+  if (key instanceof MouseEvent) {
+    key = this.html();
+  }
+
+  var ref = database.ref('drawings/' + key);
+  ref.once('value', oneDrawing, errData);
+
+  function oneDrawing(data) {
+    var dbdrawing = data.val();
+    drawing = dbdrawing.drawing;
+    //console.log(drawing);
+  }
+}
+
+function clearDrawing() {
+  drawing = [];
+}
+
+function askPrompt(){
+  bgcolor = prompt("What do you want the background color to be as(example red, blue, green, black, etc)", "white");
+  strokeinp = prompt("What do you want the stroke color to be", "black");
+  strokeWeightinp = prompt("What to you want the thickness oif the stroke to be", 5)
 }
